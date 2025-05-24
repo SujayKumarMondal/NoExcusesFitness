0c17e6b02e77e1213bf7ae0a3d3824e623de895b
SujayKumarMondal <sujay@surelia.info>
2025-05-24 08:43:09 +0530

    Add MIT License

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    if len(sys.argv) == 1 or sys.argv[1] == "runserver":
+        sys.argv = [sys.argv[0], "runserver", "127.0.0.1:2025"]
+    execute_from_command_line(sys.argv)
